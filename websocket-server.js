63039021ec3b299389e8fbc5fd16bbe56253ce15
Nestor Bermudez <nestor.bermudez@agilityfeat.com>
2015-06-19 09:54:06 -0600

    CS-89 Move signaling server outside of chatshow main server

---
+++ websocket-server.js
@@ -0,0 +1,23 @@
+var initWebsocketServer = function(httpServer) {
+  var io = require('socket.io').listen(httpServer);
+
+  io.sockets.on('connection', function(socket) {
+    console.log('user connected!');
+    socket.on('joinRoom', function(room) {
+      console.log('user joined room', room);
+      socket.join(room);
+    });
+
+    socket.on('mySnapshot', function(data) {
+      io.to(data.sessionId).emit('userSnapshot', data);
+    });
+
+    socket.on('disconnect', function() {
+      console.log('user disconnected!');
+    });
+  });
+
+  return io;
+};
+
+module.exports = initWebsocketServer;
